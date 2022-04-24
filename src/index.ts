d42c64eb5cf91bf76c261b3ddbdbb7963f2bb566
Tom Planche <tomplanche@icloud.com>
2022-04-24 23:24:58 +0200

    Site Daft V2 (multi-pages) points indiquant page en cours à terminer

--- src/index.ts
+++ src/index.ts
@@ -1,1 +1,42 @@
 // here you put all the js you want.
+//
+
+import gsap from "gsap";
+
+gsap.registerPlugin(ScrollTrigger);
+gsap.registerPlugin(ScrollToPlugin);
+
+
+
+let changeName = document.querySelector("#change");
+
+let toWrite = "DAFT"
+let timeout = 1000;
+
+let changeTimeout = () => {
+  timeout = Math.floor(Math.random() * (4300 - 500 + 1) ) + 500;
+}
+
+let write = () => {
+	let len = changeName.innerHTML.length;
+	let listeToWrite = toWrite.length;
+	
+	
+	if (len < listeToWrite) {
+		changeName.innerHTML += toWrite[len];
+		changeTimeout();
+	} else {
+		changeName.innerHTML = "";
+		toWrite = toWrite === "DAFT" ? "PUNK" : "DAFT";
+	}
+	changeTimeout()
+	
+};
+
+write();
+setInterval(write, 500);
+
+
+
+
+
